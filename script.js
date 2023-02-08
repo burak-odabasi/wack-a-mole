243776b5dffadac1b5458a84ce9732d786eb5de2
Augustcode99 <burakodabasi99@yahoo.com>
2023-02-09 02:43:59 +0300

    fixed bugs

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-var moleHere;
+let moleHere;
+let score = document.querySelector("span");
 
 let molePopUp = function () {
-  let randomSquare = Math.floor(Math.random() * 29);
-  let squareNumber = `.cell0` + randomSquare;
-  console.log(squareNumber);
-  moleHere = document.querySelector(squareNumber);
+  let prevSquare;
+  let randomNumber = Math.floor(Math.random() * 29);
+  let squareClass = `.cell0` + randomNumber;
+  moleHere = document.querySelector(squareClass);
+
+  while (moleHere === prevSquare) {
+    if (moleHere.classList.contains("hit")) {
+      moleHere.classList.remove("hit");
+    } else if (moleHere.classList.contains("miss")) {
+      moleHere.classList.remove("miss");
+    }
+  }
+  randomNumber = Math.floor(Math.random() * 29);
+  squareClass = `.cell0` + randomNumber;
+  moleHere = document.querySelector(squareClass);
+
+  prevSquare = moleHere;
+  console.log(squareClass);
   moleHere.classList.add("mole");
 };
 molePopUp();
 //----------------------------------------------------
-
 let tryHit = function () {
   console.log("trying hit");
-  const tryArray = ["hit", "miss"];
-  let thisTry = tryArray[Math.floor(Math.random() * 2)];
-
+  const tryArray = ["hit", "miss", "hit"];
+  let thisTry = tryArray[Math.floor(Math.random() * 3)];
   let prevHit = document.querySelector(".hit");
   let prevMiss = document.querySelector(".miss");
-  if (prevHit) prevHit.classList.remove("hit");
-  if (prevMiss) prevMiss.classList.remove("miss");
-
+  if (prevHit) {
+    prevHit.classList.remove("hit");
+  } else if (prevMiss) {
+    prevMiss.classList.remove("miss");
+  }
   moleHere.classList.replace("mole", thisTry);
   if (thisTry === "hit") {
-    let score = document.querySelector("span");
-    console.log(++score.textContent);
+    ++score.textContent;
   }
-  moleHere.classList.remove("mole");
-
   molePopUp();
 };
 
