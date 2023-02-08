e3d53b6dc49039e4067f6909c0504df9f425309b
Augustcode99 <burakodabasi99@yahoo.com>
2023-02-09 00:08:43 +0300

    finished, needs polishing

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// let dirtImage = document.createElement("img");
-// let hitImage = document.createElement("img");
+var moleHere;
 
 let molePopUp = function () {
   let randomSquare = Math.floor(Math.random() * 29);
   let squareNumber = `.cell0` + randomSquare;
   console.log(squareNumber);
-  let moleImage = document.createElement("img");
-  moleImage.setAttribute("src", "./images/mole.png");
-  let moleHere = document.querySelector(squareNumber);
-  moleHere.appendChild(moleImage);
+  moleHere = document.querySelector(squareNumber);
+  moleHere.classList.add("mole");
+};
+molePopUp();
+//----------------------------------------------------
+
+let tryHit = function () {
+  console.log("trying hit");
+  const tryArray = ["hit", "miss"];
+  let thisTry = tryArray[Math.floor(Math.random() * 2)];
+
+  let prevHit = document.querySelector(".hit");
+  let prevMiss = document.querySelector(".miss");
+  if (prevHit) prevHit.classList.remove("hit");
+  if (prevMiss) prevMiss.classList.remove("miss");
+
+  moleHere.classList.replace("mole", thisTry);
+  if (thisTry === "hit") {
+    let score = document.querySelector("span");
+    console.log(++score.textContent);
+  }
+  moleHere.classList.remove("mole");
+
+  molePopUp();
 };
 
-molePopUp();
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("mole")) {
+    tryHit();
+  }
+});
