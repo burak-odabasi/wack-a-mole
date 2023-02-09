43caa43b7565dd4e3e65ef9366863a35f1908824
Augustcode99 <burakodabasi99@yahoo.com>
2023-02-09 03:02:49 +0300

    less buggy

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
-let moleHere;
-let score = document.querySelector("span");
+document.addEventListener("DOMContentLoaded", function () {
+  let moleHere;
+  let score = document.querySelector("span");
 
-let molePopUp = function () {
-  let prevSquare;
-  let randomNumber = Math.floor(Math.random() * 29);
-  let squareClass = `.cell0` + randomNumber;
-  moleHere = document.querySelector(squareClass);
+  let molePopUp = function () {
+    let prevSquare;
+    let randomNumber = Math.floor(Math.random() * 28.9);
+    let squareClass = `.cell0` + randomNumber;
+    console.log(squareClass);
+    moleHere = document.querySelector(squareClass);
 
-  while (moleHere === prevSquare) {
-    if (moleHere.classList.contains("hit")) {
-      moleHere.classList.remove("hit");
-    } else if (moleHere.classList.contains("miss")) {
-      moleHere.classList.remove("miss");
+    while (moleHere === prevSquare || !moleHere.classList) {
+      randomNumber = Math.floor(Math.random() * 29);
+      squareClass = `.cell0` + randomNumber;
+      moleHere = document.querySelector(squareClass);
+      if (moleHere && moleHere.classList.contains("hit")) {
+        moleHere.classList.remove("hit");
+      } else if (moleHere && moleHere.classList.contains("miss")) {
+        moleHere.classList.remove("miss");
+      }
     }
-  }
-  randomNumber = Math.floor(Math.random() * 29);
-  squareClass = `.cell0` + randomNumber;
-  moleHere = document.querySelector(squareClass);
-
-  prevSquare = moleHere;
-  console.log(squareClass);
-  moleHere.classList.add("mole");
-};
-molePopUp();
-//----------------------------------------------------
-let tryHit = function () {
-  console.log("trying hit");
-  const tryArray = ["hit", "miss", "hit"];
-  let thisTry = tryArray[Math.floor(Math.random() * 3)];
-  let prevHit = document.querySelector(".hit");
-  let prevMiss = document.querySelector(".miss");
-  if (prevHit) {
-    prevHit.classList.remove("hit");
-  } else if (prevMiss) {
-    prevMiss.classList.remove("miss");
-  }
-  moleHere.classList.replace("mole", thisTry);
-  if (thisTry === "hit") {
-    ++score.textContent;
-  }
+    prevSquare = moleHere;
+    console.log(squareClass);
+    moleHere.classList.add("mole");
+  };
   molePopUp();
-};
+  //----------------------------------------------------
+  let tryHit = function () {
+    console.log("trying hit");
+    const tryArray = ["hit", "miss", "hit"];
+    let thisTry = tryArray[Math.floor(Math.random() * 3)];
+    let prevHit = document.querySelector(".hit");
+    let prevMiss = document.querySelector(".miss");
+    if (prevHit) {
+      prevHit.classList.remove("hit");
+    } else if (prevMiss) {
+      prevMiss.classList.remove("miss");
+    }
+    moleHere.classList.replace("mole", thisTry);
+    if (thisTry === "hit") {
+      ++score.textContent;
+    }
+    molePopUp();
+  };
 
-document.addEventListener("click", function (event) {
-  if (event.target.classList.contains("mole")) {
-    tryHit();
-  }
+  document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("mole")) {
+      tryHit();
+    }
+  });
 });
