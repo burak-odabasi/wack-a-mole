492e5ee4a4ce0626897acdd2f71ce92a1084319c
Augustcode99 <burakodabasi99@yahoo.com>
2023-02-10 14:51:24 +0300

    fixed main bug, added cursor animation etc

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let moleHere;
-  let score = document.querySelector("span");
+let moleHere;
+let score = document.querySelector("span");
 
-  let molePopUp = function () {
-    let prevSquare;
-    let randomNumber = Math.floor(Math.random() * 28.9);
-    let squareClass = `.cell0` + randomNumber;
-    console.log(squareClass);
-    moleHere = document.querySelector(squareClass);
+let molePopUp = function () {
+  let prevSquare;
+  let randomNumber = Math.floor(Math.random() * 28) + 1;
+  let squareClass = `.cell0` + randomNumber;
+  moleHere = document.querySelector(squareClass);
 
-    while (moleHere === prevSquare || !moleHere.classList) {
-      randomNumber = Math.floor(Math.random() * 29);
-      squareClass = `.cell0` + randomNumber;
-      moleHere = document.querySelector(squareClass);
-      if (moleHere && moleHere.classList.contains("hit")) {
-        moleHere.classList.remove("hit");
-      } else if (moleHere && moleHere.classList.contains("miss")) {
-        moleHere.classList.remove("miss");
-      }
-    }
-    prevSquare = moleHere;
-    console.log(squareClass);
-    moleHere.classList.add("mole");
-  };
+  // prevSquare = moleHere;
+  console.log("prevsquare:", prevSquare);
+  console.log("moleHere:", moleHere);
+  // console.log(`squareClass is:${squareClass}`);
+  moleHere.classList.add("mole");
+};
+molePopUp();
+//----------------------------------------------------
+let tryHit = function () {
+  main = document.querySelector("main");
+  main.classList.add("hammerdown");
+  setTimeout(function () {
+    main.classList.remove("hammerdown");
+  }, 300);
+  console.log("trying hit");
+  const tryArray = ["hit", "miss", "miss"];
+  let thisTry = tryArray[Math.floor(Math.random() * 3)];
+  let prevHit = document.querySelector(".hit");
+  let prevMiss = document.querySelector(".miss");
+  if (prevHit) {
+    prevHit.classList.remove("hit");
+  } else if (prevMiss) {
+    prevMiss.classList.remove("miss");
+  }
+  moleHere.classList.replace("mole", thisTry);
+  if (thisTry === "hit") {
+    ++score.textContent;
+    let hitSound = new Audio("./sounds/hammer.mp4");
+    hitSound.play();
+  } else {
+    let missSound = new Audio("./sounds/digmiss.mp4");
+    missSound.play();
+  }
   molePopUp();
-  //----------------------------------------------------
-  let tryHit = function () {
-    console.log("trying hit");
-    const tryArray = ["hit", "miss", "miss"];
-    let thisTry = tryArray[Math.floor(Math.random() * 3)];
-    let prevHit = document.querySelector(".hit");
-    let prevMiss = document.querySelector(".miss");
-    if (prevHit) {
-      prevHit.classList.remove("hit");
-    } else if (prevMiss) {
-      prevMiss.classList.remove("miss");
-    }
-    moleHere.classList.replace("mole", thisTry);
-    if (thisTry === "hit") {
-      ++score.textContent;
-      let hitSound = new Audio("./sounds/hammer.mp4");
-      hitSound.play();
-    } else {
-      let missSound = new Audio("./sounds/digmiss.mp4");
-      missSound.play();
-    }
-    molePopUp();
-  };
+};
 
-  document.addEventListener("click", function (event) {
-    if (event.target.classList.contains("mole")) {
-      tryHit();
-    }
-  });
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("mole")) {
+    tryHit();
+  }
 });
+// document.addEventListener("click", function () {
+//   document.body.style.cursor = `url(./images/hammered.png'), auto`;
+//   setTimeout(function () {
+//     document.body.style.cursor = `url('./images/hammer.png'), auto`;
+//   }, 300);
+// });
